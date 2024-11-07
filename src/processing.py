d799824f7a20e53e83ca6c1671ee0f42ba81c91d
NikToo <niktoo@internet.ru>
2024-11-07 16:26:31 +0400

    fixed a bug related to the initial condition in the function(Параметр порядка сортировки функции имеет значение по умолчанию 'True')

--- src/processing.py
+++ src/processing.py
@@ -9,9 +9,9 @@ def filter_by_state(list_dictionary: list, state_key: str = 'EXECUTED') -> list:
     return new_dictionary
 
 
-def sort_by_date(list_dictionary: list, sort_revers: bool = False) -> list:
+def sort_by_date(list_dictionary: list, sort_revers: bool = True) -> list:
     """Функция которая принимает список словарей и необязательный параметр,
-            задающий порядок сортировки по дате False/True (по умолчанию — убывание(False))"""
+            задающий порядок сортировки по дате False/True (по умолчанию — возростание(True))"""
 
     list_dictionary = sorted(list_dictionary, key=lambda list_dictionary: list_dictionary["date"], reverse=sort_revers)
     return list_dictionary
