d909e99e2eefdd2c1e51aa5acd1eaced9638f19d
NikToo <niktoo@internet.ru>
2024-11-07 14:19:27 +0400

    creating the processing module and function: sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,10 @@ def filter_by_state(list_dictionary: list, state_key = 'EXECUTED') -> list:
         if dictionary['state'] == state_key:
             new_dictionary.append(dictionary)
     return new_dictionary
+
+
+def sort_by_date(list_dictionary: list, sort_revers = False) -> list:
+    """Функция которая принимает список словарей и необязательный параметр, задающий порядок сортировки по дате False/True (по умолчанию — убывание(False))"""
+
+    list_dictionary = sorted(list_dictionary, key=lambda list_dictionary: list_dictionary["date"], reverse=sort_revers)
+    return list_dictionary
