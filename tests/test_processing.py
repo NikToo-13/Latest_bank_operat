b4e14bf9e929c9ad0fddfda825fd158324123b2d
NikToo <niktoo@internet.ru>
2024-11-15 20:11:22 +0400

    Test of the sort_by_date function of the processing.py module. Checking sorting of identical dates.

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -6,11 +6,17 @@ import pytest
 
 
 datas = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                                             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
 ]
-
+datas1 = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+          {'id': 615064531, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+          {'id': 615064541, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+          {'id': 615064551, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
 @pytest.mark.parametrize('value, expected', [
     ('EXECUTED' , [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
@@ -25,3 +31,34 @@ datas = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.5123
 ])
 def test_filter_by_state(value, expected):
     assert filter_by_state(datas, value) == expected
+
+@pytest.mark.parametrize('value, expected',[
+    (True, [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+    (False, [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+             {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}])
+])
+def test_sort_by_date(value, expected):
+    assert sort_by_date(datas, value) == expected
+
+# Проверка сортировки одинаковых дат
+@pytest.mark.parametrize('value, expected',[
+    (True, [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064531, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 615064541, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 615064551, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+    (False, [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+             {'id': 615064531, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+             {'id': 615064541, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+             {'id': 615064551, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+             {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}])
+])
+def test_sort_by_date1(value, expected):
+    assert sort_by_date(datas1, value) == expected
--- tests/testes.py
+++ tests/testes.py
@@ -37,7 +37,10 @@ print("__________функция sort_by_date__________")
 sorts_dictionary = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
                     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+                    {'id': 615064531, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                    {'id': 615064541, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                    {'id': 615064551, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+                    ]
 print(sort_by_date(sorts_dictionary))
 
 print()
