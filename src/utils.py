fe7ec2b5953d6b4fca3a7caac0d7e6c8c679a54d
NikToo <niktoo@internet.ru>
2024-12-17 23:21:22 +0400

    Implemented recording of masks module logs to the masks.log file. Logs are written to the logs folder in the project root.

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,26 @@
 import json
+import logging
+
+from project_sys import PATH_HOME
+
+path_ = f"{PATH_HOME}/logs/utils.log"
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler(path_, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def json_to_dictionary(path_file: str) -> dict:
     """которая принимает на вход путь до JSON-файла
     и возвращает список словарей с данными о финансовых транзакциях."""
     try:
+        logger.info(f"Принимаем путь к файлу: {path_file}")
         with open(path_file, "r", encoding="utf-8") as file:
             date_file = json.load(file)
+        logger.info(f"{path_file} - прочтен успешно")
         return date_file
-    except Exception:
+    except Exception as er:
+        logger.error(f"Ошибка: {er}")
         return []
