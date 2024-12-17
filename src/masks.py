bae2eec38b480b046801fec30b48a7509c9bf3b6
NikToo <niktoo@internet.ru>
2024-12-17 22:58:06 +0400

    Implemented recording of masks module logs to the masks.log file. Logs are written to the logs folder in the project root.

--- src/masks.py
+++ src/masks.py
@@ -1,25 +1,46 @@
+import logging
+
+from project_sys import PATH_HOME
+
+path_ = f"{PATH_HOME}/logs/masks.log"
+logger = logging.getLogger("masks")
+file_handler = logging.FileHandler(path_, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(card_number: int) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску.
     В случаи ошибки возвращает ошибку"""
 
     card_number1 = str(card_number)
+    logger.info(f'Принимаем номер карты: {card_number1}')
     try:
         if len(card_number1) == 16:
             card_mask = card_number1[0:4] + " " + card_number1[4:6] + "** **** " + card_number1[12:16]
+            logger.info(f'Создаем маску для номера карты: {card_mask}')
         else:
-            raise Exception("Проверти, правильность ввода номера карты!")
+            raise Exception('Проверти, правильность ввода номера карты!')
         return card_mask
     except Exception as er:
+        logger.warning(f'Ошибка: {er}')
         return str(er)
 
 
 def get_mask_account(account: int) -> str:
     """Функция принимает на вход номер счета и возвращает его маску.
     В случаи ошибки возвращает ошибку"""
-
-    account1 = str(account)
-    if len(account1) == 20:
-        account_mask = "**" + account1[-4:]
-    else:
-        account_mask = "Проверти, правильность ввода счета!"
-    return account_mask
+    try:
+        account1 = str(account)
+        logger.info(f"Принимаем номер счета: {account1}")
+        if len(account1) == 20:
+            account_mask = "**" + account1[-4:]
+            logger.info(f"Создаем маску для номера счета: {account_mask}")
+        else:
+            raise Exception("Проверти, правильность ввода счета!")
+        return account_mask
+    except Exception as er:
+        logger.warning(f"Ошибка: {er}")
+        return str(er)
