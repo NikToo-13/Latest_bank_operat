8e65fa05a123372b1e271d4bc1dfa7696a8b9a75
NikToo <niktoo@internet.ru>
2024-12-21 17:43:21 +0400

    The read_csv function has been created which takes as input the path to the csv file and returns a list of dictionaries.

--- src/read_csvexcel.py
+++ src/read_csvexcel.py
@@ -1,3 +1,30 @@
 import csv
+import logging
+
 import pandas as pd
 
+from project_sys import PATH_HOME
+
+path_ = f"{PATH_HOME}/logs/read_csvexcel.log"
+logger = logging.getLogger("read_csvexcel")
+file_handler = logging.FileHandler(path_, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+def read_csv(path_file: str) -> dict:
+    """которая принимает на вход путь до csv-файла
+        и возвращает список словарей с данными о финансовых транзакциях."""
+    try:
+        list_transaction = []
+        logger.info(f"Принимаем путь к файлу: {path_file}")
+        with open(path_file, "r", encoding="utf-8") as st_file:
+            date_file = csv.DictReader(st_file, delimiter=";")
+            logger.info(f"Читаем файл: {path_file}")
+            for row in date_file:
+                list_transaction.append(row)
+        return list_transaction
+    except Exception as er:
+        logger.error(f"Ошибка: {er}")
+        return []
