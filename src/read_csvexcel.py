390323ab4c4484465e22b17522528b63722106da
NikToo <niktoo@internet.ru>
2024-12-23 12:33:49 +0400

    Optimization of code and error handling of the read_csvexcel.py module

--- src/read_csvexcel.py
+++ src/read_csvexcel.py
@@ -17,35 +17,69 @@ logger.setLevel(logging.DEBUG)
 def read_csv(path_file: str) -> dict:
     """которая принимает на вход путь до csv-файла
         и возвращает список словарей с данными о финансовых транзакциях."""
-    try:
-        legend_s = f' Функция: read_csv -> '
-        list_transaction = []
-        logger.info(f"{legend_s}Принимаем путь к файлу: {path_file}")
-        with (open(path_file, "r", encoding="utf-8") as st_file):
-            logger.info(f"{legend_s}Читаем файл: {path_file}")
-            date_file = csv.DictReader(st_file, delimiter=";")
-            for row in date_file:
-                dict_transaction = {
-                    "id": row["id"],
-                    "state": row["state"],
-                    "date": row["date"],
-                    "operationAmount": {
-                        "amount": row["amount"],
-                        "currency": {
-                            "name": row["currency_name"],
-                            "code": row["currency_code"]
-                        }
-                    },
-                    "description": row["description"],
-                    "from": row["from"],
-                    "to": row["to"]
-                    }
-                list_transaction.append(dict_transaction)
-        logger.info(f"{legend_s}преабразуем файл под наши функции")
-        return list_transaction
 
+    def data_to_dictionary(date_files: list) -> dict:
+        """Функция принимает список словорей и преобразует под нужный нам формат"""
+        list_transaction = []
+        #try:
+        for row in date_files:
+            dict_transaction = {
+                "id": row["id"],
+                "state": row["state"],
+                "date": row["date"],
+                "operationAmount": {
+                    "amount": row["amount"],
+                    "currency": {
+                        "name": row["currency_name"],
+                        "code": row["currency_code"]
+                    }
+                },
+                "description": row["description"],
+                "from": row["from"],
+                "to": row["to"]
+                }
+            list_transaction.append(dict_transaction)
+        logger.info(f"{legend_s}(data_to_dictionary) успешное преоброзование файла")
+        return list_transaction
+        #except Exception as er:
+        #    logger.error(f"{legend_s}(data_to_dictionary) Ошибка преоброзование файла: {er}")
+        #    return []
+
+
+    def file_writes(path_files: str, delimiters: str =',') -> list:
+        """Функция принимает путь к файлу и возвращает файл в виде листа словарей"""
+        try:
+            logger.info(f"{legend_s}(file_writes) Принимаем путь к файлу: {path_files}")
+            with (open(path_files, "r", encoding="utf-8") as st_file):
+                logger.info(f"{legend_s}(file_writes) Читаем файл: {path_files}")
+                date_file = csv.DictReader(st_file, delimiter=delimiters)
+                logger.info(f"{legend_s}(file_writes) преабразуем файл под наши функции")
+                list_transaction = data_to_dictionary(date_file)
+            return list_transaction
+        except FileNotFoundError as er:
+            logger.error(f"{legend_s}(file_writes) Ошибка: {er}")
+            return []
+        except:
+            raise ValueError('Неверный разделитель')
+
+    legend_s = f' Функция: read_csv -> '
+    try:
+        logger.info(f'{legend_s}- Обработка файла с разделителем ","')
+        list_transactions = file_writes(path_file)
+        return list_transactions
     except Exception as er:
+        logger.warning(f'{legend_s}- Обработка файла с разделителем "," - некоректна ({er})')
+        logger.info(f'{legend_s}- Обработка файла с разделителем ";"')
+        list_transactions0 = file_writes(path_file, ';')
+        return list_transactions0
+    except FileNotFoundError as er:
         logger.error(f"{legend_s}Ошибка: {er}")
+        return []
+    except ValueError as er:
+        logger.error(f"{legend_s}Ошибка: {er}")
+        return []
+    else:
+        logger.critical(f"{legend_s}Ошибка!!!")
         return []
 
 
